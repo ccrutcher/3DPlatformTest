aa21d52c54e7d1b82c7fa37d3242aba7b0cab0d0
Cole Crutcher <colejcrutcher@gmail.com>
2021-02-15 15:35:53 -0600

    Added score and death

--- Scripts/PlayerCollision.cs
+++ Scripts/PlayerCollision.cs
@@ -10,6 +10,8 @@ public class PlayerCollision : MonoBehaviour
         if (collisionInfo.collider.tag == "Obstacle")
         {
             movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+
         }
 
     }
